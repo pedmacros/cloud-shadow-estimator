95e22be6c3cdd098955f4e55d658c435c601c814
Pedro Tito Macías Roselló <pedmacros@gmail.com>
2021-11-12 17:25:35 +0100

    Included cloud recognition from paper

--- src/cloud-recognition/main.py
+++ src/cloud-recognition/main.py
@@ -1,19 +1,23 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2 as cv
+import cv2
+from cloud_recognition import cloud_recognition
 
 # Blue level threshold to mask the sky
 low_blue = np.array([100,0,0])
 high_blue = np.array([255,255,255])
 
-img_src = cv.imread("img/cumulus/img04.jpg")
-img_bw = cv.cvtColor(img_src, cv.COLOR_BGR2GRAY)
-mask = cv.inRange(img_src, low_blue, high_blue)
-res = cv.bitwise_and(img_src, img_src, mask=mask)
+img_src = cv2.imread("img/cirrus/img03.jpg")
+img_bw = cv2.cvtColor(img_src, cv2.COLOR_BGR2GRAY)
+mask = cv2.inRange(img_src, low_blue, high_blue)
+res = cv2.bitwise_and(img_src, img_src, mask=mask)
 
-cv.imshow("Original", img_src)
-cv.imshow("res", res)
+final = cloud_recognition(img_src)
 
-k = cv.waitKey(0)
+cv2.imshow("Original", img_src)
+cv2.imshow("res", final)
+
+k = cv2.waitKey(0)
+
 
 
